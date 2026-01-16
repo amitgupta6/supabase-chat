9e225a71be00604b81c274701b0ea7f138b0a500
Amit Gupta <amitgupta4b@gmail.com>
2026-01-16 11:08:38 +0530

    first load select default profile

--- app/(authed)/chat/page.tsx
+++ app/(authed)/chat/page.tsx
@@ -132,6 +132,9 @@ export default function ChatPage() {
     }
     console.log("profiles", profiles);
     setProfiles(profiles);
+    if(!selectedProfile){
+      setSelectedProfile(profiles[0]);
+    }
   }
 
   const loadMessages = async () => {
